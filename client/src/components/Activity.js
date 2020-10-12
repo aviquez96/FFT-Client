852a8da8f3c40e98bd67bd8082ad0262018abfe0
Adrian Viquez <aviquez96@gmail.com>
2020-10-12 13:51:22 -0700

    WIP

--- client/src/components/Activity.js
+++ client/src/components/Activity.js
@@ -10,36 +10,13 @@ import IconButton from '@material-ui/core/IconButton';
 import ArrowDropUpTwoToneIcon from '@material-ui/icons/ArrowDropUpTwoTone';
 import ArrowDropDownTwoToneIcon from '@material-ui/icons/ArrowDropDownTwoTone';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1
-    },
-    paper: {
-        padding: theme.spacing(3),
-        margin: "auto",
-        maxWidth: 1000
-    },
-    card: {
-        width: 65,
-        height: 65,
-        display: 'flex'
-    },
-    cardContent: {
-        display: 'flex',
-        width: '100%',
-        marginTop: 5,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-}));
-
 const Activity = () => {
     const classes = useStyles();
 
     return (
         <Box className={classes.root}>
             <Paper className={classes.paper}>
-                <Grid container xs={12} spacing={1}>
+                <Grid container>
                     <Grid item xs={2} container direction="column" alignItems="center">
                         <IconButton color="primary" aria-label="add one hour" size="small">
                             <ArrowDropUpTwoToneIcon fontSize='large'/>
@@ -55,11 +32,11 @@ const Activity = () => {
                             <ArrowDropDownTwoToneIcon fontSize='large'/>
                         </IconButton>
                     </Grid>
-                    <Grid item xs={12} sm container alignItems="center">
-                        <Grid item xs container spacing={2}>
+                    <Grid item xs={8} sm container alignItems="center">
+                        <Grid item xs container>
                             <Grid item xs>
                                 <Typography gutterBottom variant="subtitle1">
-                                    Standard license
+                                    Skill Name
                                 </Typography>
                             </Grid>
                         </Grid>
@@ -69,5 +46,30 @@ const Activity = () => {
         </Box>
     )
 }
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        margin: "auto",
+        marginTop: 20,
+        marginLeft: 150
+    },
+    paper: {
+        maxWidth: 800,
+        spacing: theme.spacing(1)
+    },
+    card: {
+        width: 65,
+        height: 65,
+        display: 'flex',
+    },
+    cardContent: {
+        display: 'flex',
+        width: '100%',
+        marginTop: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+}));
 
 export default Activity
--- client/src/pages/board.js
+++ client/src/pages/board.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import TopBar from '../components/TopBar'
 import Activity from '../components/Activity'
 
 import Container from '@material-ui/core/Container';
 
 const Board = () => {
+
+    useEffect(() => {
+        const apiUrl = 'http://localhost:5000/activities'
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    })
+
     return (
-        <Container maxWidth='xl' disableGutters="true">
+        <Container maxWidth='xl' disableGutters={true}>
             <TopBar />
             <Activity />
         </Container>
